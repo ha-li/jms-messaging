5841acec24c692c52bdac4c487c2d204cb494bc5
ha-li <ha-li@gmail.com>
2017-02-23 21:51:51 -0800

    initial check in - my jms project

---
+++ src/main/java/com/gecko/message/MyMain.java
@@ -0,0 +1,10 @@
+package com.gecko.message;
+
+/**
+ * Created by hlieu on 02/23/17.
+ */
+public class MyMain {
+   public static void main (String[] args) {
+      System.out.println ("Hellow World!");
+   }
+}
